1d01442efb84d9ecb7cc966e3057ec26ad36f8b0
Ron Aflalo <ronaf@moveo.co.il>
2023-02-28 15:27:46 +0200

    loginscreen fixes

--- features/auth/LoginScreen.tsx
+++ features/auth/LoginScreen.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { ImageBackground, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import AppInput from '../../components/AppInput'
 import AuthButton from '../../components/AuthButton';
-
+import { KeyboardAvoidingView } from 'react-native';
 interface User {
     email: string,
     password: string
@@ -12,14 +12,13 @@ const LoginScreen = () => {
     const handleLoginPressed = () => {
         console.log(1, "Login Button Pressed!");
     }
-    const [user, setUser] = useState<User>({ email: '', password: '' })
     const handleSignUpPressed = () => {
         console.log(2, "SIGNUP-> Button Pressed!");
         if (email.includes('@moveo.co.il') && password === rePassword) {
             console.log(3, email, password, rePassword)
             const USER = { email: email, password: password };
             console.log(USER);
-        }else{
+        } else {
             console.log("fix to show the problem!");
             //error message
         }
@@ -29,24 +28,36 @@ const LoginScreen = () => {
     const [rePassword, setRePassword] = useState('');
 
     return (
-        <View style={styles.container}>
-            <View style={styles.inputsContainer}>
-                <AppInput label='Your email' isPassword={false} value={email} onChange={setEmail} />
-                <AppInput label='Password' isPassword={true} value={password} onChange={setPassword} />
-                <AppInput label='Re-Enter Password' isPassword={true} value={rePassword} onChange={setRePassword} />
+        <KeyboardAvoidingView style={styles.viewsContainer}>
+            <View style={styles.container}>
+
+
+                <View style={styles.inputsContainer}>
+                    <AppInput label='Your email' isPassword={false} value={email} onChange={setEmail} />
+                    <AppInput label='Password' isPassword={true} value={password} onChange={setPassword} />
+                    <AppInput label='Re-Enter Password' isPassword={true} value={rePassword} onChange={setRePassword} />
+                </View>
+                <View style={styles.buttonsContainer}>
+                    <AuthButton label="SIGNUP ->" bgcolor={(email === '' || password === '' || rePassword === '') ? "#6CA4E1" : "#0058B9"} onPress={handleSignUpPressed} />
+                    <AuthButton label="LOGIN" bgcolor="#F1F1F9" onPress={handleLoginPressed} />
+                </View>
+
             </View>
-            <View style={styles.buttonsContainer}>
-                <AuthButton label="SIGNUP ->" bgcolor={(email === '' || password === '' || rePassword === '') ? "#6CA4E1" : "#0058B9"} onPress={handleSignUpPressed} />
-                <AuthButton label="LOGIN" bgcolor="#F1F1F9" onPress={handleLoginPressed} />
-            </View>
-        </View>
+        </KeyboardAvoidingView>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        marginTop: 250
+        gap:64,
+        marginTop: 150,
+        backgroundColor: '#F8F8FF',
+        
+
+    },
+    viewsContainer: {
+        flex:1,
+        gap: 100
     },
     buttonsContainer: {
         flex: 1,
