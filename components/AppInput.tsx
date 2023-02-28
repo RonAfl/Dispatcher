8ce7f9a0c7fde94d418324346291ba5f9b93baa9
Ron Aflalo <ronaf@moveo.co.il>
2023-02-28 12:31:09 +0200

    appInput modification

--- components/AppInput.tsx
+++ components/AppInput.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-const AppInput = () => {
+interface InputProps {
+    label: string,
+    isPassword: boolean
+}
+
+const AppInput: React.FC<InputProps> = ({ label, isPassword }) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    };
+
     return (
         <View style={styles.container} >
-            <TextInput style={styles.inputStyle} placeholder='Your email'></TextInput>
+            <TextInput style={styles.inputStyle} placeholder={label} secureTextEntry={isPasswordVisible} />
+            {isPassword && <TouchableOpacity onPress={togglePasswordVisibility}>
+                <Image
+                    style={styles.imageStyle}
+                    source={
+                        isPasswordVisible
+                            ? require('../assets/images/eye-unvisible.png')
+                            : require('../assets/images/eye-visible.png')
+                    } />
+            </TouchableOpacity>}
         </View>
     )
 };
@@ -14,23 +34,28 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        flexDirection: 'row',
         marginTop: 32,
+        width: '90%',
+        borderWidth: 1,
+        minHeight: 44,
+        borderRadius: 4,
+        padding: 8,
+        borderColor: '#5A5A89',
     },
     inputStyle: {
         flex: 1,
-        padding: 8,
-        width: '80%',
-        maxHeight: 44,
-        borderWidth: 1,
-        borderRadius: 4,
-        alignItems: 'center',
-        justifyContent: 'center',
         backgroundColor: '#FFFFFF',
-        paddingLeft: 16,
+        minHeight: 40,
+        paddingLeft: 8,
         lineHeight: 22,
         letterSpacing: 0.25,
-        borderColor: '#5A5A89',
-        textDecorationColor: '#5A5A89,'
+        textDecorationColor: '#5A5A89'
+    },
+    imageStyle: {
+        width: 20,
+        height: 20,
+        marginLeft: 8,
     }
 })
 
--- features/auth/LoginScreen.tsx
+++ features/auth/LoginScreen.tsx
@@ -14,7 +14,11 @@ const handleSignUpPressed = () => {
 const LoginScreen = () => {
     return (
         <View style={styles.container}>
-            <AppInput/>
+            <View style={styles.inputsContainer}>
+                <AppInput label='Your email' isPassword={false}/>
+                <AppInput label='Password' isPassword={true}/>
+                <AppInput label='Re-Enter Password' isPassword={true}/>
+            </View>
             <View style={styles.buttonsContainer}>
                 <AuthButton label="SIGNUP ->" bgcolor="#0058B9" onPress={handleSignUpPressed} />
                 <AuthButton label="LOGIN" bgcolor="#F1F1F9" onPress={handleLoginPressed} />
@@ -26,13 +30,19 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        marginTop: 250
     },
     buttonsContainer: {
-        flex:1,
-        alignItems:'center',
+        flex: 1,
+        alignItems: 'center',
         justifyContent: 'center',
-        flexDirection:'column',
-        gap:24,
+        flexDirection: 'column',
+        gap: 24,
+    },
+    inputsContainer:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom:52,
     }
 })
 
